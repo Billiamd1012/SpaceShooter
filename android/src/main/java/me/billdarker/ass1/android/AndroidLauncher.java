4f46b00bb26a599904a04134ca002e2bac9ad17a
Billiamd1012 <william.darker2003@gmail.com>
2025-04-05 14:10:42 +1030

    Initial project creation

---
+++ android/src/main/java/me/billdarker/ass1/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package me.billdarker.ass1.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.billdarker.ass1.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
